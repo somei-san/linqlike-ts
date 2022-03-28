172f959a7935ddae7834aa08a434e507002b4920
somei <somei_tanoue@jp.hodna>
2022-03-28 16:26:32 +0900

    GithubActionのSecret名を変更

--- src/tests/linq.test.ts
+++ src/tests/linq.test.ts
@@ -3,9 +3,8 @@ import "../lib/linq";
 test(`インスタンス化できる!`, () => {
   var list = [1, 9, 2, 8, 3, 7, 4, 6, 5, 1];
 
-  expect(list.only((x) => x === 9)).toBe(true);
+  expect(list.first((x) => x === 1)).toBe(1);
 });
-
 
 var list = [1, 9, 2, 8, 3, 7, 4, 6, 5, 1];
 
@@ -22,6 +21,7 @@ try {
   console.log("れいがいだよ！" + e);
 }
 
+/*
 type Sample = {
   value: number;
 };
@@ -30,3 +30,4 @@ var test1: Sample = { value: 1 };
 var test2: Sample = { value: 2 };
 
 if (test1 == test2) throw new Error("");
+*/
