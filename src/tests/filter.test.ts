31def89150fbedb2b7fac4e4c395129a95d74787
somei <10me1goya@gmail.com>
2023-06-18 07:07:02 +0900

    🗑️ 不要な記述を削除

--- src/tests/filter.test.ts
+++ src/tests/filter.test.ts
@@ -8,29 +8,3 @@ const actual = list.filterNotNullish()
   expect(actual).toBeInstanceOf(Array<number>);
   expect(actual).toStrictEqual([1, 2, 3, 4, 5, 6]);
 });
-
-var list = [1, 9, 2, 8, 3, 7, 4, 6, 5, 1];
-
-console.log(list);
-
-console.log(list.reduce((p, x) => p + x));
-
-console.log(list.find((x) => x == 50));
-console.log(list.every((x) => x < 20));
-
-try {
-  console.log(list.single((x) => x == 1));
-} catch (e) {
-  console.log("れいがいだよ！" + e);
-}
-
-/*
-type Sample = {
-  value: number;
-};
-
-var test1: Sample = { value: 1 };
-var test2: Sample = { value: 2 };
-
-if (test1 == test2) throw new Error("");
-*/
